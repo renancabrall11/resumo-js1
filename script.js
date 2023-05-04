499a560bc1ee642e49313acc9c21ea66718009d1
renancabral <you@example.com>
2023-05-04 11:46:47 -0300

    resumo

---
+++ script.js
@@ -0,0 +1,28 @@
+function Confirmar()
+{
+    texto = nome.value
+    teste.innerHTML = texto
+}
+
+function MostrarJanela()
+{
+    janelinha.style.display = "block"
+}
+
+function FecharJanela()
+{
+    janelinha.style.display = "none"
+}
+
+soma=0
+function Somar()
+{
+    soma = soma + 1
+    resultado.value = soma
+}
+
+function Imprimir()
+{
+    window.print();
+    BT.style.display = "none"
+}
